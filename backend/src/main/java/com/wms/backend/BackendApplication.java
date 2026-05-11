6302938eea8b203b6a2503a648bbbcb48e35d126
LonlyEdward <lonlyedward@gmail.com>
2026-05-12 00:22:24 +0300

    set up docker and created flyway migration files

--- backend/src/main/java/com/wms/backend/BackendApplication.java
+++ backend/src/main/java/com/wms/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.wms.backend;
 
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+////		make spring boot read my .env file and set all key value pairs and set them as java system properties
+//		Dotenv dotenv = Dotenv.configure()
+//						.directory("../")
+//						.ignoreIfMissing()
+//						.load();
+//
+//		dotenv.entries().forEach(entry ->
+//				System.setProperty(entry.getKey(), entry.getValue())
+//				);
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
